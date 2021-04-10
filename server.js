2c9ecc9c27536ec2bac5ce7274da2ade9d611179
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-04-10 16:54:36 -0600

    added a html route

--- server.js
+++ server.js
@@ -7,10 +7,20 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.urlencoded({extend: true}));
 app.use(express.json);
-app.use(require('./routes/workouts-api'))
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+app.use(require('./routes/workouts-api'));
+app.usr(require('./routes/html-api'));
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port http://localhost:${PORT}!`);
+})
+
+
 
