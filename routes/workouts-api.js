948a21f7880a63a6f09cb0a6edf962daa6835243
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-04-11 21:07:28 -0600

    updated routhing

--- routes/workouts-api.js
+++ routes/workouts-api.js
@@ -1,17 +1,63 @@
 const router = require('express').Router();
 const path = require('path');
 const db = require('../models');
+const {Workout} = require('../models');
 
-router.get('/api/worksouts-api', (req, res) => {
-    db.Workout.find({}).then(workout => {
-        res.json(workout)
+router.get('/api/worksouts', (req, res) => {
+    db.Workout.find({}).then((data) => {
+        res.json(data)
     }).catch (err => {
         res.json(err);
     });
 });
 
-// router.get ('/api/workouts', (req, res) => {
-//     let workoutId = req.params.id;
-//     let exerciseWorkouts = workout[0].
-// })
+router.post('/api/workouts', ({body}, res) => {
+    db.Workout.create(body).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err)
+    })
+});
+
+router.get('/api/workouts/:id', (req, res) => {
+    Workout.findByIdAndUpdate(
+        {
+            _id: req.params.id
+        },
+        {
+            $push: {
+                exercise: req.body
+            }
+        }
+    ).then((dbWorkout) => {
+        console.log(dbWorkout);
+        res.json(dbWorkout);
+    }).catch((err) => {
+        res.status(400).json(err)
+    });
+});
+
+router.get('.api/workouts/', (req, res) => {
+    Workout.find({}).sort({date: -1})
+    .then((dbWorkout) => {
+        console.log(dbWorkout);
+        res.json(dbWorkout);
+    }).catch((err) => {
+        res.status(400).json(err);
+    });
+});
+
+router.get('.api/workouts/:range', (req, res) => {
+    Workout.find({}).then((dbWorkout) => {
+        res.json(dbWorkout);
+    }).catch((err) => {
+        res.status(400).json(err)
+    });
+});
+
+module.exports = router;
+
+
+
+
 
--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 var MONGODB = process.env.MONGODB_URI || "mongodb://localhost/workout";
+const db =require("./models")
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(logger("dev"));
+
+app.use(express.static("public"));
+
+
 app.use(express.urlencoded({extend: true}));
 app.use(express.json);
 
+app.use(require('./routes/workouts-api'));
+app.usr(require('./routes/html-api'));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
-app.use(require('./routes/workouts-api'));
-app.usr(require('./routes/html-api'));
 
 
 app.listen(PORT, () => {
